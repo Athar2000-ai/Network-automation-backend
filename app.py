a42a8390deeb23eb15ea9b7f6058cedb54f4f779
Muhammad Athar Waqas <atharwaqas619@gmail.com>
2025-06-20 01:19:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+
 # from netmiko import ConnectHandler  # Uncomment when using real devices
 
 app = Flask(__name__)
@@ -59,6 +61,5 @@ def configure_device():
 # Add this at the bottom of app.py
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
