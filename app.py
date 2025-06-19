e95723e91f3311898d9771177ed77de64f15de16
Muhammad Athar Waqas <atharwaqas619@gmail.com>
2025-06-20 01:08:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,6 @@ def configure_device():
 # Add this at the bottom of app.py
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
